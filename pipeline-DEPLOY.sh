f84958a921b2f2acb994823786fdfc9e08d86bff
sachinkj <sachinkj@in.ibm.com>
2017-08-17 13:44:33 +0530

    initial commit

---
+++ pipeline-DEPLOY.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# The default Node.js version is 0.10.40
+# To use Node.js 0.12.7, uncomment the following line:
+#export PATH=/opt/IBM/node-v0.12/bin:$PATH
+# To use Node.js 4.2.2, uncomment the following line:
+export PATH=/opt/IBM/node-v4.2/bin:$PATH
+#npm install
+echo $PWD
+ls -al .
+
+echo "env variables are:"
+echo ${REPO_BRANCH}
+echo ${APIC_URL_US}
+echo ${SPACE}
+echo ${APIC_USERNAME}
+echo ${CF_APP_NAME}
+echo ${REGION}
+
+echo " APIC URL IS  ${APIC_URL_US}"
+echo "org is ${ORG} "
+
+# apic api url
+APIC_API_URL=api.${APIC_URL_US}
+
+# install yaml
+if [ ! -f ./yaml ]; then
+    wget https://github.com/mikefarah/yaml/releases/download/1.5/yaml_linux_amd64 -O yaml
+    chmod +x yaml
+fi
+
+
+#create cloudant service instance 
+cf create-service cloudantNoSQLDB Lite hotels-events-db
+# create a key for this service
+cf create-service-key hotels-events-db for-api
+# retrieve the URL - it contains credentials + API URL
+CLOUDANT_URL=`cf service-key hotels-events-db for-api | grep \"url\" | awk -F '"' '{print $4}'`
+CLOUDANT_DATABASE="eventsdb"
+curl $CLOUDANT_URL/$CLOUDANT_DATABASE -X PUT
+#ignore db already exists message
+
+echo "Updating datasources.json with cloudant url"
+CLOUDANT_URL=$(echo $CLOUDANT_URL | sed -e 's/\//\\\//g'  )
+sed -i .bak  -e "s/\"url\":.*/\"url\":\"$CLOUDANT_URL\",/" server/datasources.json
+cat server/datasources.json
+
+# set orgs
+CHARLST="[@|.|-|_]"
+ORGS=$(echo $ORG|sed "s/$CHARLST//g")-$SPACE
+ORGS=$(echo $ORGS|tr '[:upper:]' '[:lower:]')
+echo "orgs is $ORGS"
+
+# Configure APIC cli
+echo -e "Start installing apiconnect cli"
+npm install -g apiconnect
+echo -e "completed installing apic cli"
+echo "yes" | apic
+echo "no" | apic
+
+echo -e "Login and config set catalog.."
+apic login -u $APIC_USERNAME -p $APIC_PASSWORD -s ${APIC_URL_US}
+apic config:set catalog=apic-catalog://${APIC_URL_US}/orgs/${ORGS}/catalogs/sb
+apic organizations -s us.apiconnect.ibmcloud.com
+#apic config:set app=apic-app://${APIC_URL_US}/orgs/${ORGS}/apps/$CF_APP_NAME
+#apic apps:publish
+cf push $CF_APP_NAME
+./yaml w --inplace definitions/onboarding-event-api.yaml x-ibm-configuration.catalogs.sb.properties.runtime-url "https://${CF_APP_NAME}.mybluemix.net"
+cat  definitions/onboarding-event-api.yaml
+
+
+apic publish -o ${ORGS} -s ${APIC_URL_US} definitions/onboarding-event-api-product.yaml
+
